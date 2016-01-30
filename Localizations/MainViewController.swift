d47edb6426534b51653ff3c95a3392f59da42370
Arnaud Thiercelin <athiercelin@icloud.com>
2016-01-30 17:49:46 -0600

    Continuous implementation of the string generation/cleanup
    Project cleanup.

--- Localizations/MainViewController.swift
+++ Localizations/MainViewController.swift
@@ -12,6 +12,10 @@ class MainViewController: NSViewController {
 
 	@IBOutlet weak var chooseButton: NSButton!
 	var rootDirectory: NSURL!
+	// TODO: Add here a random cache directory generator
+	let cacheDirectory = "/tmp/"
+
+	var xibFiles = [NSString]()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -48,7 +52,7 @@ class MainViewController: NSViewController {
 			//			do {
 			//				try self.rootDirectory.bookmarkDataWithOptions(NSURLBookmarkCreationOptions.SecurityScopeAllowOnlyReadAccess, includingResourceValuesForKeys: nil, relativeToURL: nil)
 			
-			dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), { () -> Void in
+			dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), { [unowned self] () -> Void in
 				
 				// TODO: to be implemented to show what was found
 				// search for localization files starting at root
@@ -64,7 +68,9 @@ class MainViewController: NSViewController {
 				// build fresh list of generated files.
 				
 				// show detail ui
-				//				self.performSegueWithIdentifier("detailSegue", sender: sender)
+				dispatch_async(dispatch_get_main_queue(), { [unowned self] () -> Void in
+				NSLog("show something")
+				})
 			})
 			//			} catch {
 			//           // TODO: Implement error here if and when this app is to be sandboxed
@@ -73,11 +79,70 @@ class MainViewController: NSViewController {
 	}
 	
 	func generateFreshFilesUsingGenstrings() {
-		
+		let commandString = "/usr/bin/genstrings -o \(self.cacheDirectory) `find \(self.rootDirectory.path!) -name '*.[hm]'`"
+
+		system(commandString)
 	}
 	
 	func generateFreshFilesWithIBTool() {
+		//find . -name '*.xib'
+		// ibtool --generate-strings-file MainMenu.strings MainMenu.xib
+
+		self.xibFiles.removeAll()
+		self.findAllXibFiles(self.rootDirectory.path!)
 		
+		for filePath in self.xibFiles {
+			
+		//		NSArray *filePathComponents = [filePath pathComponents];
+			let pathExtension = filePath.pathExtension
+			let fileName = filePath.lastPathComponent
+			let stringFileName = fileName.stringByReplacingOccurrencesOfString(pathExtension, withString: "strings")
+
+			let commandString = "ibtool --generate-strings-file \(self.cacheDirectory)\(stringFileName) \(filePath)"
+		
+			system(commandString)
+		}
+	}
+	
+	func findAllXibFiles(startPath: NSString) {
+		//NOTE: Could be replaced by fts_open from libc (man)
+		
+		let fileManager = NSFileManager.defaultManager()
+		do {
+			let content = try fileManager.contentsOfDirectoryAtPath(startPath as String)
+			
+			for element in content {
+				let elementPath = startPath.stringByAppendingPathComponent(element)
+				var isDirectory: ObjCBool = false
+				
+				fileManager.fileExistsAtPath(elementPath, isDirectory: &isDirectory)
+				if isDirectory {
+					// Skipping Directories that can't be open
+					if fileManager.isExecutableFileAtPath(elementPath) {
+						continue
+					}
+					
+					// Skipping Hidden Folders
+					let dotRange = element.rangeOfString(".")
+					if dotRange != nil && dotRange!.first! == element.startIndex {
+						continue
+					}
+					
+					// We "open" the folder and continue the process.
+					self.findAllXibFiles(elementPath)
+				}
+				else // files - we are only interested in localizations files.
+				{
+					let xibRange = element.rangeOfString(".xib")
+					
+					if xibRange != nil && self.xibFiles.contains(elementPath) == false {
+						self.xibFiles.append(elementPath)
+					}
+				}				
+			}
+		} catch {
+			// TODO: Error Handling here
+		}
 	}
 	
 	func produceFreshListOfStringFiles() {
